950f645e1afe2523159b6446f0da16338b548597
vinay pandey <vinaypandey0801gmail.com>
2024-06-14 21:04:29 -0700

    Final touch to About US page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function about () {
   return (
+    <>
     <div>about us page </div>
+    <ol>
+      <ls>  <Link href="/about/Yash">Yash page</Link></ls>
+      <ls>  <Link href="/about/Vaibhav">Vaibhav page</Link></ls>
+  
+   
+    </ol>
+    </>
+
   )
 }
